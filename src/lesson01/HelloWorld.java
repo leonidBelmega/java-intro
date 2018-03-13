9d16949a5eba476f41d9c16b8dd0fbdaedb6dc87
Leonid Belmega <collector.sharing@gmail>
2018-03-13 19:27:40 +0200

    comment

---
+++ src/lesson01/HelloWorld.java
@@ -0,0 +1,21 @@
+package lesson01;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world" + " " + "!");
+    boolean b = true;
+    int i    = 2;
+    long l   = 5120003456L;
+    float f  = 9.5678f;
+    double d =9.5;
+    char c   = 'F';
+        System.out.println(b);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(i);
+    }
+
+}
