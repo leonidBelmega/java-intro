401e79ce13f971328766defcbece64633ec8ea40
leonidBelmega <37343924+leonidBelmega@users.noreply.github.com>
2018-03-17 16:02:47 +0200

    Update EveryNth.java

--- EveryNth.java
+++ EveryNth.java
@@ -10,8 +10,7 @@ public class EveryNth {
     static void eachNumber(String text, int peroid) {
         System.out.println(text);
         char[] charArray = text.toCharArray();
-        //System.out.print(charArray[0]);
-
+       
         for (int i = 0; i < charArray.length; i++) {
             if ((i + 1) % peroid == 0 || i == 0) {
                 System.out.print(charArray[i]);
