9da172ae6981bba1f19e57ff71ea398a1b805dbe
Leonid Belmega <collector.sharing@gmail>
2018-03-22 18:54:11 +0200

    lesson04

--- src/lesson03/composition/Insect.java
+++ src/lesson03/composition/Insect.java
@@ -10,5 +10,25 @@ class Insect {
         this.size = size;
         this.color = color;
     }
+
+    public static class AttackImpl implements Attack {
+        private  String move;
+        private String attack;
+
+        public  AttackImpl (String move, String attack) {
+            this.move = move;
+            this.attack = attack;
+        }
+        @Override
+        public void move() {
+            System.out.println(move);
+        }
+        @Override
+        public void attack() {
+            move();
+            System.out.println(attack);
+        }
+
+    }
 }
 
---
+++ src/lesson04/classWork/Collections.java
@@ -0,0 +1,27 @@
+package lesson04.classWork;
+
+import java.util.ArrayList;
+
+public class Collections {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Yellow");
+        colors.add("Green");
+        colors.add("White");
+        colors.add("Blue");
+        colors.remove(0);
+        ArrayList<Integer> digits = new ArrayList<>();
+        digits.add(12);
+        digits.add(13);
+        digits.add(14);
+        digits.add(15);
+        digits.add(16);
+        digits.remove(digits.get(0));
+
+
+
+
+
+    }
+}
